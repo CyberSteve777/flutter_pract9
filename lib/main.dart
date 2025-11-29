c1827012bb733cdc101c3b84b268509436aa5b77
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-11-29 17:00:21 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'screens/auth_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/students_screen.dart';
-import 'screens/courses_screen.dart';
-import 'screens/teachers_screen.dart';
-import 'screens/grades_screen.dart';
-import 'screens/profile_screen.dart';
+import 'package:pract9/bloc/courses/courses_bloc.dart';
+import 'package:pract9/bloc/grades/grades_bloc.dart';
+import 'package:pract9/bloc/home/home_bloc.dart';
+import 'package:pract9/bloc/profile/profile_bloc.dart';
+import 'package:pract9/bloc/students/students_bloc.dart';
+import 'package:pract9/bloc/teachers/teachers_bloc.dart';
+import 'bloc/auth/auth_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'services/data_service.dart';
 import 'services/auth_service.dart';
 import 'package:get_it/get_it.dart';
+import 'routes.dart';
 
 void main() {
   GetIt.I.registerSingleton<DataService>(DataService());
@@ -22,52 +23,32 @@ class EducationalSystemApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final router = GoRouter(
-      initialLocation: '/login',
-      routes: [
-        GoRoute(
-          path: '/login',
-          builder: (context, state) => const AuthorizationScreen(),
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(create: (_) => AuthBloc()),
+          BlocProvider<CoursesBloc>(create: (_) => CoursesBloc()),
+          BlocProvider<GradesBloc>(create: (_) => GradesBloc()),
+          BlocProvider<HomeBloc>(create: (_) => HomeBloc()),
+          BlocProvider<ProfileBloc>(create: (_) => ProfileBloc()),
+          BlocProvider<StudentsBloc>(create: (_) => StudentsBloc()),
+          BlocProvider<TeachersBloc>(create: (_) => TeachersBloc()),
+        ],
+        child: MaterialApp.router(
+          title: 'Образовательная система',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+          ),
+          debugShowCheckedModeBanner: false,
+          routerConfig: AppRouter.createRouter(),
         ),
-        GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
-        GoRoute(
-          path: '/students',
-          builder: (context, state) => const StudentsScreen(),
-        ),
-        GoRoute(
-          path: '/courses',
-          builder: (context, state) => const CoursesScreen(),
-        ),
-        GoRoute(
-          path: '/teachers',
-          builder: (context, state) => const TeachersScreen(),
-        ),
-        GoRoute(
-          path: '/grades',
-          builder: (context, state) => const GradesScreen(),
-        ),
-        GoRoute(
-          path: '/profile',
-          builder: (context, state) => const ProfileScreen(),
-        ),
-      ],
-    );
-
-    return MaterialApp.router(
-      title: 'Образовательная система',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
     );
   }
 }
